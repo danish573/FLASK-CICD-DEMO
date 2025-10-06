2e9bafd22667d921776331d388c4b46e5341c76c
danish573 <dkhan573@gmail.com>
2025-10-06 17:35:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return "Hello DevOPs, Flask CI/CD"
+    return "Hello DevOPs, 
+            If you see me, you had successfully deploy app to port 80"
 
 @app.route("/healthz")
 def healthz():
     return jsonify({"Status":"ok"}), 200
 
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port=5000, debug=False)
